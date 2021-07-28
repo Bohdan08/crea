4cdd496148e2efbc60c51095fba9857581fdd513
Bohdan881 <6228238-Bohdan881@users.noreply.gitlab.com>
2021-07-28 17:01:23 -0400

    fix: working on biils

--- pages/bills/index.js
+++ pages/bills/index.js
@@ -4,7 +4,21 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { API_PAGE_SIZE, API_US_GOV_INFO_BILLS } from "../../shared/constants";
 import styled from "styled-components";
 
-const StyledCardContainer = styled.div``;
+const StyledCardContainer = styled.div`
+  &:hover {
+    border-color: lightseagreen;
+  }
+
+  &:hover .icon-arrow {
+    color: lightseagreen;
+  }
+`;
+
+const StyledFontAwesomeIconArrowRight = styled(FontAwesomeIcon)`
+  top: 8px;
+  right: 5px;
+  color: transparent;
+`;
 
 const InnerInfoBox = ({ title, description }) => (
   <div className="border-2 w-3/12 mx-2 text-xl  p-2 rounded text-center">
@@ -29,34 +43,6 @@ const Bills = ({ data }) => {
     };
   });
 
-  // console.log(document, "document");
-  // const lastComponent = document.getElementById("bills-list");
-  // console.log(lastComponent, "lastComponent");
-  const handleScroll = () => {
-    // To get page offset of last user
-    // const lastUserLoaded = document.querySelector(
-    //   ".user-list > .user:last-child"
-    // );
-    // if (lastUserLoaded) {
-    //   const lastUserLoadedOffset =
-    //     lastUserLoaded.offsetTop + lastUserLoaded.clientHeight;
-    //   const pageOffset = window.pageYOffset + window.innerHeight;
-    //   // Detects when user scrolls down till the last user
-    //   if (pageOffset > lastUserLoadedOffset) {
-    //     // Stops loading
-    //     if (userData.curPage < userData.maxPage) {
-    //       // Trigger fetch
-    //       const query = router.query;
-    //       query.page = parseInt(userData.curPage) + 1;
-    //       router.push({
-    //         pathname: router.pathname,
-    //         query: query,
-    //       });
-    //     }
-    //   }
-    // }
-  };
-
   return (
     <div className="my-12 mx-auto max-w-screen-lg px-10">
       <div className="mb-2">
@@ -70,97 +56,79 @@ const Bills = ({ data }) => {
       <div className="flex flex-row flex-wrap justify-start" id="bills-list">
         {data?.packages?.length &&
           data.packages.map(
-            ({ packageId, congress, title, lastModified, dateIssued }, key) => {
+            ({ packageId, congress, title, lastModified, dateIssued }) => {
               const formatDate = (dateStr) =>
                 new Date(dateStr).toString().slice(4, 15);
 
               return (
-                <div
-                  key={packageId}
-                  className="py-2 
-                  my-2 w-full"
-                >
-                  {/* >
+                <div key={packageId} className="bill-item py-2 my-2 w-full">
                   <Link
-                    href="bills/billd/[packageId]/[packageId]"
-                    as={`posts/post/${packageId}/packageId`}
-                  > */}
-                  <div
-                    className="post-container_  py-2 border-2 bg-white rounded-lg text-lg 
-                  "
+                    href="bills/bill/[packageId]"
+                    as={`bills/bill/${packageId}`}
                   >
-                    <StyledCardContainer className="w-full">
-                      <div className="px-4">
-                        {/* <FontAwesomeIcon
-                      className="h-6 w-6 post-arrow"
-                      icon="arrow-right"
-                    /> */}
-                        <p className="text-xl font-light pl-2">{title}</p>
-                        <div className="flex w-full mt-5 justify-around">
-                          <InnerInfoBox
-                            title="Congress"
-                            description={congress}
-                          />
-                          <InnerInfoBox
-                            title="Introduced"
-                            description={formatDate(dateIssued)}
-                          />
-                          <InnerInfoBox
-                            title="Last Modified"
-                            description={formatDate(lastModified)}
-                          />
-                          <InnerInfoBox
-                            title="Comments"
-                            description={getRandomInt(2000)}
-                          />
-                        </div>
-                        <div className="flex w-full mt-5 justify-around">
+                    <StyledCardContainer className="cursor-pointer relative py-4 border-2 bg-white rounded-lg text-lg">
+                      <StyledFontAwesomeIconArrowRight
+                        className="h-6 w-6 absolute icon-arrow"
+                        icon="arrow-right"
+                      />
+                      <div className="w-full">
+                        <div className="px-4">
+                          <p className="text-xl font-light pl-2">{title}</p>
+                          <div className="flex w-full mt-5 justify-around">
+                            <InnerInfoBox
+                              title="Congress"
+                              description={congress}
+                            />
+                            <InnerInfoBox
+                              title="Introduced"
+                              description={formatDate(dateIssued)}
+                            />
+                            <InnerInfoBox
+                              title="Last Modified"
+                              description={formatDate(lastModified)}
+                            />
+                            <InnerInfoBox
+                              title="Comments"
+                              description={getRandomInt(2000)}
+                            />
+                          </div>
+                          {/* <div className="flex w-full mt-5 justify-around">
                           <button className="w-3/12 mx-1 rounded-full py-2 border-2 border-gray-600 cursor-pointer text-center rounded focus:outline-none focus:ring-2">
                             {" "}
                             Summary{" "}
-                          </button>
-                          <button className="w-3/12 mx-1 rounded-full py-2 border-2 border-gray-600 cursor-pointer text-center rounded hover:border-black focus:outline-none focus:ring-2">
-                            {" "}
-                            Full Text{" "}
-                          </button>
-                          <button className="w-3/12 mx-1 rounded-full py-2 border-2 border-gray-600 cursor-pointer text-center rounded hover:border-black focus:outline-none focus:ring-2">
-                            {" "}
-                            Details{" "}
-                          </button>
-                          <button className="w-3/12 mx-1 rounded-full py-2 border-2 border-gray-600 cursor-not-allowed text-center rounded opacity-70 focus:outline-none focus:ring-2">
-                            {" "}
-                            Your vote{" "}
-                          </button>
+                          </button> 
+                        </div> */}
                         </div>
-                      </div>
-                      <div className="flex w-full items-center justify-left border-t-2 pl-6 mt-4">
-                        <div className="flex flex-row mt-2">
-                          <div className="flex flex-row">
-                            <button className="rounded-full flex items-center border p-2 bg-blue-600 focus:outline-none focus:ring-2">
-                              <FontAwesomeIcon icon="thumbs-up" color="white" />
-                            </button>
-                            <span className="text-gray-500 mt-1 ml-1">
-                              {getRandomInt(100)}
-                            </span>
-                          </div>
+                        <div className="flex w-full items-center justify-left border-t-2 pl-6 mt-4">
+                          <div className="flex flex-row mt-4">
+                            <div className="flex flex-row">
+                              <button className="rounded-full flex items-center border p-2 bg-blue-600 focus:outline-none focus:ring-2">
+                                <FontAwesomeIcon
+                                  icon="thumbs-up"
+                                  color="white"
+                                />
+                              </button>
+                              <span className="text-gray-500 mt-1 ml-1">
+                                {getRandomInt(100)}
+                              </span>
+                            </div>
 
-                          <div className="flex flex-row ml-3">
-                            <button className="rounded-full flex items-center border p-2 bg-red-600 focus:outline-none focus:ring-2">
-                              <FontAwesomeIcon
-                                icon="thumbs-down"
-                                color="white"
-                              />
-                            </button>
-                            <span className="text-gray-500 mt-1 ml-1">
-                              {getRandomInt(100)}
-                            </span>
+                            <div className="flex flex-row ml-3">
+                              <button className="rounded-full flex items-center border p-2 bg-red-600 focus:outline-none focus:ring-2">
+                                <FontAwesomeIcon
+                                  icon="thumbs-down"
+                                  color="white"
+                                />
+                              </button>
+                              <span className="text-gray-500 mt-1 ml-1">
+                                {getRandomInt(100)}
+                              </span>
+                            </div>
                           </div>
                         </div>
                       </div>
                     </StyledCardContainer>
-                  </div>
-
-                  {/* </Link> */}
+                  </Link>
                 </div>
               );
             }
